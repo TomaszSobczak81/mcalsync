36de6d2caa70b013fe962879d85974846b5d7776
tomassob <tomeksobczak@gmail.com>
2024-07-02 22:04:14 +0200

    Added SpringBoot skeleton

---
+++ src/main/java/com/sbczk/mcalsync/McalsyncApplication.java
@@ -0,0 +1,13 @@
+package com.sbczk.mcalsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McalsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McalsyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sbczk/mcalsync/McalsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbczk.mcalsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McalsyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
